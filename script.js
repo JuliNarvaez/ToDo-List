13c95f9f23b98c76557d8ddf3548a969f1517083
JuliNarvaez <0720jano@gmail.com>
2020-09-26 20:09:57 -0500

    Json elements

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const elements = document.querySelectorAll(".item");
+function render(data) {
+    data.forEach(function (datos) {
+        const card = `<p class="item--title">${datos.title} ${datos.id}</p>
+        <p class="item--details">${datos.details}</p>
+        <div class="item--checkbox">
+            <label>
+                <input type="checkbox" name="" id="cbox1">
+            </label>
+            <p>Complete</p>
+        </div>
+    `;
 
-elements.forEach(element => {
-    const checkbox = element.querySelector("input");
-    checkbox.addEventListener('click', () => {
-        element.classList.toggle("checked-accept");
+        const container = document.querySelector(".container__content");
+        const div = document.createElement('div');
+        div.innerHTML = card;
+        div.className = "item";
+        container.appendChild(div);
     })
-})
+
+    const elements = document.querySelectorAll(".item");
+
+    elements.forEach(element => {
+        const checkbox = element.querySelector("input");
+        checkbox.addEventListener('click', () => {
+            element.classList.toggle("checked-accept");
+        })
+    });
+};
+
+fetch('./data/MOCK_DATA.json')
+    .then(response => response.json())
+    .then(render);
+    
