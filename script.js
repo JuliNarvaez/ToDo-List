2fa318de35028267d35831cea0cf7b13a87c443d
JuliNarvaez <0720jano@gmail.com>
2020-09-28 19:31:21 -0500

    Pseudo filter

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-function render(data) {
-    data.forEach(function (datos) {
-        const card = `<p class="item--title">${datos.title} ${datos.id}</p>
-        <p class="item--details">${datos.details}</p>
-        <div class="item--checkbox">
-            <label>
-                <input type="checkbox" name="" id="cbox1">
-            </label>
-            <p>Complete</p>
-        </div>
-    `;
+const container = document.querySelector(".container__content");
 
-        const container = document.querySelector(".container__content");
-        const div = document.createElement('div');
-        div.innerHTML = card;
-        div.className = "item";
-        container.appendChild(div);
-    })
-
-    const elements = document.querySelectorAll(".item");
-
-    elements.forEach(element => {
-        const checkbox = element.querySelector("input");
+function render(e) {
+    e.forEach(toDo => {
+        const template = `
+                <p class="item--title">${toDo.title} ${toDo.id}</p>
+                <p class="item--details">${toDo.destails}</p>
+                <div class="item--checkbox">
+                    <label>
+                        <input type="checkbox" name="" id="cbox1">
+                    </label>
+                    <p>Complete</p>
+                </div>
+        `;
+        const card = document.createElement('div');
+        card.innerHTML = template;
+        card.className = "item";
+        const checkbox = card.querySelector("input");
+        checkbox.checked = toDo.done;
+        checkbox.checked ? card.classList.add('checked-accept') : card.classList.remove('checked-accept');
         checkbox.addEventListener('click', () => {
-            element.classList.toggle("checked-accept");
+            card.classList.toggle("checked-accept");
         })
-    });
-};
+        container.appendChild(card);
+    })
+}
 
 fetch('./data/MOCK_DATA.json')
     .then(response => response.json())
-    .then(render);
-    
+    .then(filtrar);
+
+const filterButtons = document.querySelectorAll("button");
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle("button-pressed");
+        filtrar([],button.classList[0]);
+    });
+});
+
+function filtrar(f) {
+    var complete = f.filter(function (done) {
+        switch ("button-all") {
+            case "button-all":
+                return done.done == done.done;
+            case "button-completed":
+                return done.done == true;
+            case "button-incomplete":
+                return done.done == false;
+            }
+    })
+    render(complete);
+};
+
+ 
