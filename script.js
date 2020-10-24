01d6c54a0b293afe902818ada5f0eee6336bbea4
JuliNarvaez <0720jano@gmail.com>
2020-10-24 18:32:03 -0500

    All ajustments in a row
    q
    qwe
    w
    
    Q
    
    X

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container__content");
 
-function render(e) {
-    e.forEach(toDo => {
+function render(todos) {
+    todos.forEach(toDo => {
         const template = `
                 <p class="item--title">${toDo.title} ${toDo.id}</p>
                 <p class="item--details">${toDo.destails}</p>
@@ -15,9 +15,10 @@ function render(e) {
         const card = document.createElement('div');
         card.innerHTML = template;
         card.className = "item";
+
         const checkbox = card.querySelector("input");
         checkbox.checked = toDo.done;
-        checkbox.checked ? card.classList.add('checked-accept') : card.classList.remove('checked-accept');
+        card.classList.toggle('checked-accept', toDo.done);
         checkbox.addEventListener('click', () => {
             card.classList.toggle("checked-accept");
         })
@@ -30,32 +31,26 @@ let variableArr;
 fetch('./data/MOCK_DATA.json')
     .then(response => response.json())
     .then(data => {
-        filterAll(data, "button-all")
         variableArr = data;
+        render(data);
     });
 
 const filterButtons = document.querySelectorAll("button");
 
-filterButtons.forEach(button => {
-    button.addEventListener('click', () => { 
-        filterButtons.forEach(btn => {
-            btn.classList.remove("button-pressed");
-        });
-        button.classList.toggle("button-pressed");
-        container.innerHTML = "";
-        filterAll(variableArr, button.classList[0]);
+function filterAll(element) {
+    filterButtons.forEach(button => {
+        button.classList.remove("button-pressed");
     });
-});
-
-function filterAll(f, b) {
-    var complete = f.filter(function (done) {
-        switch (b) {
-            case "button-all":
-                return done.done == done.done;
-            case "button-completed":
-                return done.done == true;
-            case "button-incomplete":
-                return done.done == false;
+    element.classList.add("button-pressed");
+    container.innerHTML = "";
+    var complete = variableArr.filter(function ({done}) {
+        switch (element.value) {
+            case "all":
+                return done;
+            case "completed":
+                return done == true;
+            case "incomplete":
+                return done == false;
         }
     });
     render(complete);
